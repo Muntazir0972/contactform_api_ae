1b9abf34b7ee71759f074de721253bccdcd01a07
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-06-13 16:07:00 +0500

    first commit

---
+++ app/Http/Controllers/Api/ContactFormController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+
+class ContactFormController extends Controller
+{
+     // Store Contact Form data
+     public function ContactForm(Request $request) {
+ 
+        // Form validation
+         $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone'=>'required',
+            'company'=>'required',
+            'subject' => 'required',
+            'message' => 'required'
+         ]);
+         if ($validator->fails()) { 
+     return response()->json(['error'=>$validator->errors()], 401);            
+ }
+        //  Store data in database
+        // Contact::create($request->all());
+ 
+        //  Send mail to Application Admin
+        \Mail::send('emails.contact', array(
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'phone' => $request->get('phone'),
+            'company' => $request->get('company'),
+            'bodyMessage' => $request->get('message'),
+        ), function($message) use ($request){
+            $message->from($request->email);
+            $message->to('info@indenta.ai', 'Admin')->subject($request->get('subject'));
+        });
+        return response()->json(['success' => 'The email has been sent.']);
+    }
+}
---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,8 @@
+<title>Contact Form Submission</title>
+ 
+<strong>Name : </strong>{{ $name }} <br>
+<strong>Email : </strong> {{ $email }} <br>
+<strong>Phone : </strong>{{ $phone }} <br>
+<strong>Company : </strong>{{ $company }} <br>
+<strong>Message : </strong>{{ $bodyMessage }} <br><br>
+<i><strong>Thanks</strong></i>
